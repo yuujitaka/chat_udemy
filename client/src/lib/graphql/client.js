336fd793d9e73e7bccb7190a9b2ab36fae569397
yuujitaka <rebecacleviane@gmail.com>
2024-11-11 13:42:48 +0100

    add ws on client

--- client/src/lib/graphql/client.js
+++ client/src/lib/graphql/client.js
@@ -1,19 +1,47 @@
-import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient as createWsClient } from 'graphql-ws';
 import { getAccessToken } from '../auth';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
 
-const httpLink = createHttpLink({ uri: 'http://localhost:9000/graphql' });
+const isSubscription = (operation) => {
+  const definition = getMainDefinition(operation.query);
+  return (
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    definition.operation === OperationTypeNode.SUBSCRIPTION
+  );
+};
 
 const authLink = new ApolloLink((operation, forward) => {
   const accessToken = getAccessToken();
   if (accessToken) {
     operation.setContext({
-      headers: { 'Authorization': `Bearer ${accessToken}` },
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
   }
   return forward(operation);
 });
 
+const httpLink = concat(
+  authLink,
+  createHttpLink({ uri: 'http://localhost:9000/graphql' })
+);
+
+const wsLink = new GraphQLWsLink(
+  createWsClient({
+    url: 'ws://localhost:9000/graphql',
+  })
+);
+
 export const apolloClient = new ApolloClient({
-  link: concat(authLink, httpLink),
+  link: split(isSubscription, wsLink, httpLink),
   cache: new InMemoryCache(),
 });
